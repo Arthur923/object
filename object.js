bec93f2a763b3866fa7b2aeb0d77c45de5c9c289
Arthur923 <54887865+Arthur923@users.noreply.github.com>
2020-04-16 18:57:13 +0300

    Update object.js

--- object.js
+++ object.js
@@ -1,15 +1,15 @@
-var city1 = {
-    name: "Kyiv",
-    population: 100000000,
+var I = {
+    name: "Tallin",
+    population: 30000000,
     GetName: function(){console.log(this.name)},
     exportStr: function(){console.log(this.name,this.population)}
 };
 
-var city2 = {
-    name: "Vinnitsya",
-    population: 10**6,
+var II = {
+    name: "Lviv",
+    population: 43**60,
     GetName: function(){console.log(this.name)},
     exportStr: function(){console.log(this.name,this.population)}
 };
 
-city1.test="dgdg";
+city1.test="12345";
